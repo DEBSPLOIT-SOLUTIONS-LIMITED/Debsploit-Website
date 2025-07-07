b8eebdb725139bab6367ba28b0ebf74cbfcb7baf
Brandon <brandonochieng72@gmail.com>
2025-07-07 17:43:47 +0300

    Updated requirements.txt to accomordate for versions

--- debsploit_solutions/settings.py
+++ debsploit_solutions/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-here'
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
-
+    
 
 # Reminder: Nafaa Kuedit Email Configuration
 
